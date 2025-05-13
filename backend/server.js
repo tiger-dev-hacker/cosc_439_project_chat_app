02d5b2d1391cd1fafa422bc042313e69803ca283
Suril Waghmare <waghmaresuril@gmail.com>
2025-05-12 21:18:06 -0400

    deployment test

--- backend/server.js
+++ backend/server.js
@@ -33,23 +33,23 @@ app.use('/api/message', messageRoutes);
 
 // --------------------------deployment------------------------------
 
-// const __dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname1, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
     
     
     
-//   console.log("Registering wildcard route: *");
+  console.log("Registering wildcard route: *");
 
-//   app.get("/*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("/*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // --------------------------deployment------------------------------
 
